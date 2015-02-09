6e33442dc41574b63c4258d66afeadf9b1495ad8
Wick <wickbox@gmail.com>
2015-02-08 22:21:01 -0800

    make some pieces

--- CURRY.py
+++ CURRY.py
@@ -3,7 +3,34 @@
 
 
 
-pieces = []
+pieceList = []
+
+
+for team in range(0,2):
+	for pawns in range(0,8):
+		pawn = Pawn()
+		pawn.initialize(team, pawns)
+		pieceList.append(pawn)
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
