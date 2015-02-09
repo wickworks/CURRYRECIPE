335cc05a82bffb2338cef559419ffd030b060f69
Wick <wickbox@gmail.com>
2015-02-08 22:42:54 -0800

    victorrryyyyy

--- CURRY.py
+++ CURRY.py
@@ -1,5 +1,5 @@
 
-import prawncurry.py
+import prawncurry
 
 
 
@@ -8,18 +8,32 @@ pieceList = []
 
 for team in range(0,2):
 	for pawns in range(0,8):
-		pawn = Pawn()
+		pawn = prawncurry.Pawn()
 		pawn.initialize(team, pawns)
 		pieceList.append(pawn)
 		
 		
 
+def drawBoard():
+	print("")
+	for y in range(0,8):
+		for x in range(0,8):
+			empty = 1
+			
+			for piece in pieceList:
+				if piece.x == x and piece.y == y:
+					print("[" + str(piece.team) + "]"),
+					empty = 0
+					
+			if empty == 1:
+				print("[ ]"),
+		
+		print("")
 
 
 
 
-
-
+drawBoard()
 
 
 
